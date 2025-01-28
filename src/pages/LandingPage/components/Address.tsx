72e5b8070433868c7d729cff14684412b6d8e042
Omkar Nighot <94929580+omkarnighot@users.noreply.github.com>
2025-01-28 12:58:07 +0530

    reduces size of map on mobile

--- src/pages/LandingPage/components/Address.tsx
+++ src/pages/LandingPage/components/Address.tsx
@@ -24,7 +24,7 @@ const Address = () => {
                 />
                 <iframe
                     src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3780.642493025884!2d73.82051489999999!3d18.6351432!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bc2b9004d92db5f%3A0x4646c7ae62103c09!2sTara%20Health%20Consultations!5e0!3m2!1sen!2sin!4v1736543816439!5m2!1sen!2sin"
-                    width="400"
+                    width="350"
                     height="350"
                     loading="lazy"
                     className="md:hidden"
